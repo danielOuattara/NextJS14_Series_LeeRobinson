c9c444b756c97bbfa6ec5bc8b4938cd94a248b74
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-15 15:42:44 +0100

    01 intro reactjs nextjs: 01 strat project from scratch

---
+++ 01_intro_to_ReactJs_NextJs/01_start_next_js_project_from_scratch/app/components/Header.jsx
@@ -0,0 +1,8 @@
+export function Header({ subject }) {
+  return (
+    <header>
+      <h1>This is the header</h1>
+      <h2>Lesson about {subject}</h2>
+    </header>
+  );
+}
---
+++ 01_intro_to_ReactJs_NextJs/01_start_next_js_project_from_scratch/app/page.jsx
@@ -0,0 +1,14 @@
+import { Header } from "./components/Header";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Header subject="Next.js" />
+      <h2>Main page</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit ullam
+        quibusdam non.
+      </p>
+    </div>
+  );
+}
